be726f78b8fa41dece6d45f96ce39e0ca136acff
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2023-07-28 12:06:39 -0400

    Update setup.sh

--- Ubuntu22_04/setup.sh
+++ Ubuntu22_04/setup.sh
@@ -3,14 +3,6 @@ echo "Setting up script..."
 
 apt install python3 -y
 apt install python3-pip -y
-pip install os
-pip install sys
-pip install logging
-pip install platform
-pip install subprocess
-pip install psutil
-pip install crontab
-pip install pexpect
-pip install re
+pip install requirements.txt
 
 echo "Finished setting up!"
