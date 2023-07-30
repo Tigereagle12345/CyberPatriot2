7ce333ea2d9eb505b4589334f37ff7f2a5c312da
Tigereagle12345 <pythonisgreat17@gmail.com>
2023-07-30 16:45:23 -0400

    Update start.sh

--- Ubuntu22_04/start.sh
+++ Ubuntu22_04/start.sh
@@ -9,14 +9,12 @@ apt install python3-pip -y
 
 #sudo -u $(logname) pip install -r "$DIRECTORY/requirements.txt"
 
-sudo -u $(logname) pip3 install os
 sudo -u $(logname) pip3 install argparse
-sudo -u $(logname) pip3 install sys
-sudo -u $(logname) pip3 install platform
-sudo -u $(logname) pip3 install subprocess
+sudo -u $(logname) pip3 install os-sys
+sudo -u $(logname) pip3 install lib-platform
 sudo -u $(logname) pip3 install psutil
 sudo -u $(logname) pip3 install crontab
 sudo -u $(logname) pip3 install pexpect
-sudo -u $(logname) pip3 install re
+sudo -u $(logname) pip3 install regex
 
 echo "Finished setting up!"
