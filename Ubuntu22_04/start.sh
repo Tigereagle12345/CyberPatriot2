f51c92aade8dbf4d66d0a7c3b5441f6d523a6e99
Tigereagle12345 <pythonisgreat17@gmail.com>
2023-07-31 16:41:50 -0400

    Updated start.sh

--- Ubuntu22_04/start.sh
+++ Ubuntu22_04/start.sh
@@ -4,15 +4,11 @@ echo "Setting up script..."
 apt install python3 -y
 apt install python3-pip -y
 
-#PWD=$(pwd)
-#DIRECTORY=$(cd `dirname $0` && pwd)
-
-#sudo -u $(logname) pip install -r "$DIRECTORY/requirements.txt"
-
 sudo -u $(logname) pip3 install argparse
 sudo -u $(logname) pip3 install os-sys
 sudo -u $(logname) pip3 install lib-platform
-sudo -u $(logname) pip3 install psutil
+pip3 install psutil
+pip3 install --upgrade psutil
 sudo -u $(logname) pip3 install crontab
 sudo -u $(logname) pip3 install pexpect
 sudo -u $(logname) pip3 install regex
