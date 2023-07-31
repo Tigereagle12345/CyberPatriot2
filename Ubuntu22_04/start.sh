61034f4b3aa70905351a673ae405319826916085
Tigereagle12345 <pythonisgreat17@gmail.com>
2023-07-31 16:44:18 -0400

    Update start.sh

--- Ubuntu22_04/start.sh
+++ Ubuntu22_04/start.sh
@@ -9,7 +9,7 @@ sudo -u $(logname) pip3 install os-sys
 sudo -u $(logname) pip3 install lib-platform
 pip3 install psutil
 pip3 install --upgrade psutil
-sudo -u $(logname) pip3 install crontab
+pip3 install crontab
 sudo -u $(logname) pip3 install pexpect
 sudo -u $(logname) pip3 install regex
 
