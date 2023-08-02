a92ec3eb67e4b5ce6d9deace30068f8f2ddd0e0d
Tigereagle12345 <pythonisgreat17@gmail.com>
2023-08-01 20:53:53 -0400

    Update start.sh

--- Ubuntu22_04/start.sh
+++ Ubuntu22_04/start.sh
@@ -7,7 +7,7 @@ apt install python3-pip -y
 sudo -u $(logname) pip3 install argparse
 sudo -u $(logname) pip3 install os-sys
 sudo -u $(logname) pip3 install lib-platform
-pip3 install crontab
+sudo -u $(logname) pip3 install python-crontab
 sudo -u $(logname) pip3 install pexpect
 sudo -u $(logname) pip3 install regex
 
