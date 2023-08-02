ebbfa390c007db640aec0e4adac5042c7f0e1750
Tigereagle12345 <pythonisgreat17@gmail.com>
2023-08-02 17:59:00 -0400

    Update start.sh

--- Ubuntu22_04/start.sh
+++ Ubuntu22_04/start.sh
@@ -5,7 +5,7 @@ apt install python3 -y
 apt install python3-pip -y
 
 sudo -u $(logname) pip3 install argparse
-sudo -u $(logname) pip3 install os-sys
+#sudo -u $(logname) pip3 install os-sys
 sudo -u $(logname) pip3 install lib-platform
 pip3 uninstall crontab
 pip3 install python-crontab
