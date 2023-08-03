e07cc7753473a314d5c2193a09547c98bdfd551a
Tigereagle12345 <pythonisgreat17@gmail.com>
2023-08-03 16:32:42 -0400

    Update start.sh

--- Ubuntu22_04/start.sh
+++ Ubuntu22_04/start.sh
@@ -4,12 +4,12 @@ echo "Setting up script..."
 apt install python3 -y
 apt install python3-pip -y
 
-sudo -u $(logname) pip3 install argparse
+pip3 install argparse
 #sudo -u $(logname) pip3 install os-sys
-sudo -u $(logname) pip3 install lib-platform
+pip3 install lib-platform
 pip3 uninstall crontab
 pip3 install python-crontab
-sudo -u $(logname) pip3 install pexpect
-sudo -u $(logname) pip3 install regex
+pip3 install pexpect
+pip3 install regex
 
 echo "Finished setting up!"
